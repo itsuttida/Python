58f442f458b5626b1b7df6fcdbf7f1c1aa5b5b58
Suttida Satjasunsern <suttida.s@teohong.com>
2021-03-21 12:46:11 +0700

    โจทย์

---
+++ if.py
@@ -0,0 +1,31 @@
+# if boolean expression :
+#     statement
+
+#age = 15 - 20 วัยรุ่น
+# 21 - 29 ผู้ใหญ่
+# 30 - 39 วัยทำงาน
+
+#and or not
+
+age = int(input("age : "))
+
+name = input("name : ")
+
+# if age >= 15 and age <= 20:
+#     print("วัยรุ่น")
+
+# if age >= 21 and age <= 29 :
+#     print("ผู้ใหญ่")
+
+# if age >= 30 and age <= 39:
+#     print("วัยทำงาน")
+
+# if not (age == 15) :
+#     print("age != 15")
+
+# print(age == 15)
+
+if name == "aew" :
+    print("aew")
+else:
+    print("nick")
---
+++ ternary.py
@@ -0,0 +1,16 @@
+
+# age >= 15 วัยรุ่น
+# ไม่ใช่ 
+
+
+age = int(input("อายุของคุณ  : "))
+
+# if age >= 15 :
+#     print("วัยรุ่น")
+# else :
+#     print("วัยเด็ก")
+
+
+# เงื่อนไขเป็นจริง if expression else "เงื่อนไขอื่นๆ"
+
+print("วัยรุ่น") if age >= 15 else print("วัยเด็ก")
