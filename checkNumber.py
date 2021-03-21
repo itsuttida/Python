a7e214e6f351ee0e5c459049d7848ec7b1286058
thekxin <80328391+thekxin@users.noreply.github.com>
2021-03-21 13:05:14 +0700

    Update checkNumber.py

--- checkNumber.py
+++ checkNumber.py
@@ -3,3 +3,10 @@
 
 #2  = 2 is even
 #5  = 5 is odd
+
+number = int (input( " enter number " ))
+if number % 2 == 0 :
+    print( " %d is even " %(number))
+
+else : 
+    print( " %d is odd" %(number))
