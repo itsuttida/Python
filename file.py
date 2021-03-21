7dc1f08e8b52c56e1c2c74c327184c32baa811ba
thekxin <80328391+thekxin@users.noreply.github.com>
2021-03-21 12:08:12 +0700

    Create file.py

---
+++ file.py
@@ -0,0 +1,10 @@
+print(3)
+#1 1 50 = 3710
+#2 2 30 = 7350
+
+hr = int (input(" จำนวนชั่วโมง "))
+minute = int (input(" จำนวนนาที "))
+sec = int (input(" จำนวนวินาที "))
+
+number = (hr * 3600) + (minute * 60) + (sec)
+print(number)
