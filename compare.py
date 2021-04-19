a6777e9757bb220bfd0a3298437345a402558bbf
Suttida Satjasunsern <suttida.s@teohong.com>
2021-04-19 09:55:43 +0700

    Update compare.py

--- compare.py
+++ compare.py
@@ -5,7 +5,7 @@ num1 = int(input("ป้อนตัวเลขที่ 1 "))
 num2 = int(input("ป้อนตัวเลขที่ 2 "))
 
 
-if num1 > num2:
+if num1 > num2 :
     print(num1 , "มากกว่า", num2)
 else :
     print(num1 , "น้อยกว่า", num2)
