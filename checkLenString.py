51d4c4add7ebfec729cd043a8bfcf22263c2ec2c
Suttida Satjasunsern <suttida.sat@gmail.com>
2021-04-25 15:24:46 +0700

    Create checkLenString.py

---
+++ checkLenString.py
@@ -0,0 +1,15 @@
+#เขียนโปรแกรมรับ input 2 ตัว ที่เป็นตัวอักษร
+#ตรวจสอบว่าทั้ง 2 มีีความยาวเท่ากันไหม
+#ถ้าใช่ให้ print Same
+#ถ้าไมเท่าให้ print Not Same
+
+
+str1 = input("Enter string 1 : ")
+str2 = input("Enter String 2 : ")
+
+print(len(str1))
+print(len(str2))
+
+# if str1 == str2:
+#     print("Same")
+    
